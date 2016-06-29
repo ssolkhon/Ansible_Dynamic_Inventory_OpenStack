b0129e733c613471f609e2ac74dce116c811b2ac
Scott Solkhon <scottsolkhon@gmail.com>
2016-06-29 21:57:06 +0100

    updated readme and added dynamic inventory file for openstack

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Ansible_Dynamic_Inventory_OpenStack
-Dynamic Inventory file for Ansible that gathers information about servers in OpenStack.
+# Ansible Dynamic Inventory OpenStack
+The dynamic_inventory_openstack.py file will allow you to dynamically populate an Ansible inventory file using metadata tags on OpenStack instances.
+
+***Before using this dynamic inventory file, you will need to modify the 'regions' variable on line 36 to reflect the region(s) that you are targeting in your OpenStack setup.***
+
+#### Host Groups
+To set host groups you can add the 'ansible_host_groups' metadata tag to the openstack instances that you would like to target:
+
+```
+$ nova meta <serevr-id> set "ansible_host_groups=<ansible-group-name>" 
+```
+
+You can set multiple host groups using a comma to separate groups:
+
+```
+$ nova meta <serevr-id> set "ansible_host_groups=<ansible-group-name1>,<ansible-group-name2>,<ansible-group-name3>" 
+```
+
+#### Host Variables
+To set host variables add a metadata tag for 'ansible_host_vars' to your instances:
+
+```
+$ nova meta <serevr-id> set "ansible_host_vars=<key>:<value>" 
+```
+
+You can set multiple host groups using a comma to separate groups:
+
+```
+$ nova meta <serevr-id> set "ansible_host_vars=<key1>:<value1>,<key2>:<value2>,<key3>:<value3>" 
+```
+
+To ensure that your instances are being picked up by the dynamic inventory file you can simply run the python script and examine the output:
+
+```
+$ source <your-openstack-rc-file>.sh
+$ python dynamic_inventory_openstack.py
+```
+
+You can then use the dynamic inventory file with the 'ansible-playbook' commad using a '-i' flag.
---
+++ dynamic_inventory_openstack.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+from novaclient import client as nova_client
+import os
+import simplejson as json
+
+def get_nova_session(region):
+    '''
+    Returns a nova client connection to selected region
+    '''
+    nova_session = nova_client.Client('2',
+                                      os.environ['OS_USERNAME'],
+                                      os.environ['OS_PASSWORD'],
+                                      os.environ['OS_TENANT_NAME'],
+                                      os.environ['OS_AUTH_URL'],
+                                      region_name=region,
+                                      connection_pool=True
+                                      )
+
+    return nova_session
+
+def get_servers(session, srv_name=None, net_name=None):
+    '''
+    Returns a list of servers 
+    '''
+    if srv_name is None and net_name is None:
+        return session.servers.list()
+    else:
+        if net_name is not None and srv_name is None:
+            net_filter = filter(lambda servers: servers.networks.keys()[0] == net_name, get_servers(session))
+            return net_filter
+
+        srv_filter = filter(lambda servers: servers.name == srv_name, get_servers(session, net_name=net_name))
+        return srv_filter
+
+def main():
+    regions = ['REG01', 'REG02']
+    inventory = {}
+    inventory['_meta'] = {"hostvars" : {}} 
+
+    for region in regions:
+      nova_session = get_nova_session(region) 
+      servers = get_servers(nova_session)
+      for server in servers:  
+          if server.metadata.has_key('ansible_host_groups'):
+            tags = server.metadata['ansible_host_groups'].split(',')
+            for tag in tags:
+              if inventory.has_key(tag):
+                inventory[tag]['hosts'].append(server.networks.items()[0][1][0])
+              else:
+                inventory[tag] = {"hosts" : [server.networks.items()[0][1][0]]}
+
+          if server.metadata.has_key('ansible_host_vars'):
+            hostvars = server.metadata['ansible_host_vars'].split(',')
+
+            for hostvar in hostvars:
+              key, value = hostvar.split(':')
+              if inventory['_meta']['hostvars'].has_key(server.networks.items()[0][1][0]):
+                  inventory['_meta']['hostvars'][server.networks.items()[0][1][0]][key] = value
+              else:
+                  inventory['_meta']['hostvars'][server.networks.items()[0][1][0]] = {}
+                  inventory['_meta']['hostvars'][server.networks.items()[0][1][0]][key] = value
+
+
+
+    my_json = json.dumps(inventory, sort_keys=True, indent=4 * ' ')
+    print my_json
+
+
+if __name__ == "__main__":
+    main()
